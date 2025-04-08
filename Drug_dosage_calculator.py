f85b1b5f7ae691a2bdf976d93bc5b51b4824f353
Apple <huangduduo@hotmail.com>
2025-04-08 15:42:14 +0800

    Calculator

---
+++ Drug_dosage_calculator.py
@@ -0,0 +1,18 @@
+wei = float(input('Please input weight in kg: (number only)'))
+stren = input('Please input strength of paracetamol (A=120mg/5ml or B=250mg/5ml):')
+def calculate_dosage(weight,strength):
+    dose=15
+    if weight<10 or weight>100:
+        return ('Error weight')
+    if strength=='120mg/5ml' or strength=='A':
+        stre=120
+    elif strength=='250mg/5ml'or strength=='B':
+        stre=250
+    else:
+        return ('Error strength of paracetamol')
+    dosage=weight*dose/stre
+    b='The drug dosage is '+str(dosage)+' ml.'
+    return (b)
+a=calculate_dosage(wei,stren)
+print (a)
+    
